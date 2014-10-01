7d0cc40c3c570e069497f1170480dab2aa4ed84a
gs512 <g.saldi@gmail.com>
2014-10-01 09:08:03 -0400

    Added tools scripts

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.db.models import get_app, get_models
 # Register your models here.
+from library.models import *
+
+for model in get_models(get_app("library")):
+	admin.site.register(model)
--- wings/urls.py
+++ wings/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wings.views.home', name='home'),
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^library/', include('library.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'library/login.html'}),
 )
