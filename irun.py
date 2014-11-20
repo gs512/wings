23ec7cec336195252e486efb0644e24ffc1cbe8f
gs512 <g.saldi@gmail.com>
2014-11-20 23:31:09 +0400

    import read_groups

--- irun.py
+++ irun.py
@@ -15,3 +15,25 @@ for i in range(len(s)):line=s[i].split('\t');r = read_group_tracking(project=p,t
 # 	condition = models.CharField(max_length=100)
 # 	replicate = models.IntegerField(default=0)
 # 	FPKM = models.FloatField(default=0)
+s=open("/Users/gs/Desktop/CL/gene_exp.diff","r").read().splitlines()[1:]
+for v in s:
+# 	line=v.replace(',',"_").split('\t')
+	line = v.split('\t')
+	cond=str(line[4]+","+line[5])
+	cond_1=line[4]
+	cond_2=line[5]
+	gene=line[0]
+	inf=str(line[9])
+	FC=0.0
+	PV=float(line[11])
+	fpkm_1=float(line[7])
+	fpkm_2=float(line[8])
+	if "inf" in inf:
+		inf=True
+		fpkm_1=fpkm_1+1
+		fpkm_2=fpkm_2+1
+		FC=math.log((fpkm_2/fpkm_1),2)
+	else :FC=float(inf);inf=False;
+
+	g=gene_exp(test_id=gene,sample_1=cond_1,sample_2=cond_2,p_value=PV,FC=FC,fpkm_1=fpkm_1,fpkm_2=fpkm_2,q_value=line[13],SEM_1=stats.sem(read_group_tracking.objects.get(project=p,tracking_id=gene,condition=cond_1).value('FPKM')),SEM_2=stats.sem(read_group_tracking.objects.get(project=p,tracking_id=gene,condition=cond_2).value('FPKM')))
+	g.save()
